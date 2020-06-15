a3361b193cfa56cf69af3fb77df174d84ad3dc55
Anbumani <anbuathypathy@gmail.com>
2020-06-15 22:36:32 +0530

    Firebase comp changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 import { Employee } from './employee.model';
 import { AppService } from './app.service';
 
@@ -12,25 +12,40 @@ export class AppComponent {
   title = 'demo-ui-app';
   empForm: FormGroup;
   employee: Employee = new Employee();
-  displayedColumns: string[] = ['id', 'name', 'age'];
-  dataSource:Employee[] = [];
-  constructor(private appService:AppService){}
-  ngOnInit(){
-    this.appService.getAllEmployees().subscribe(employees =>
-      this.dataSource = employees);
-    this.empForm = new FormGroup({
-      id: new FormControl("",Validators.required),
-      name: new FormControl("",Validators.required),
-      age: new FormControl("",Validators.required) 
-    }
-    );
-  }
-  saveEmployee(employee: Employee){
-    this.appService.saveEmployee(employee).subscribe(result =>{
-      console.log(result);
-      
+  displayedColumns: string[] = ['id', 'name', 'age','read','write','speak'];
+  dataSource: Employee[] = [];
+  laguageArray: FormArray;
+  constructor(private appService: AppService, private formBuilder: FormBuilder) { }
+  ngOnInit() {
+    this.appService.getAllEmployees().subscribe(data => {
+      this.dataSource = data.map(employees => {
+        return {
+          id: employees.payload.doc.data()['id'],
+          name: employees.payload.doc.data()['name'],
+          age: employees.payload.doc.data()['age']
+        } as Employee;
+      })
     });
-    this.appService.getAllEmployees().subscribe(employees =>
-      this.dataSource = employees);
+
+    this.empForm = new FormGroup({
+      id: new FormControl("", Validators.required),
+      name: new FormControl("", Validators.required),
+      age: new FormControl("", Validators.required),
+    });
   }
+  
+  saveEmployee(employee: Employee) {
+    this.appService.saveEmployee(employee);
+    this.appService.getAllEmployees().subscribe(data => {
+      this.dataSource = data.map(employees => {
+        return {
+          id: employees.payload.doc.data()['id'],
+          name: employees.payload.doc.data()['name'],
+          age: employees.payload.doc.data()['age']
+        } as Employee;
+
+      })
+    });
+  }
+
 }
